3a706c72335dc0549d387fbfcf70ccb235a9520e
unknown <you@example.com>
2026-08-24 10:53:43 +0700

    Lab02 A

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Lab02
+﻿/*
+ * Student ID : 1690700305
+ * Name       : น.ส.ณัชชาทองหมื่นศรี
+ * Section    : 129A
+ * No.        : N/A
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+
+namespace Lab02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string bossName = "Kirin";
+            char rank = 'S';
+            int level = 7;
+            int maxHp = 240;
+            int currentHp = 175;
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine();
+
+            int hpPercent = currentHp * 100 / maxHp;
+            Console.WriteLine($"HP Percent: {hpPercent}%");
+            Console.WriteLine();
+            Console.WriteLine("Kirin takes 60 damage!");
         }
     }
 }
