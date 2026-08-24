33a06254262acc6b79b64f915858c400602ab5e6
unknown <you@example.com>
2026-08-24 11:15:43 +0700

    Lab02 A complete

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -7,6 +7,8 @@
  */
 
 
+using static System.Net.Mime.MediaTypeNames;
+
 namespace Lab02
 {
     internal class Program
@@ -23,12 +25,25 @@ namespace Lab02
             bool isBoss = true;
 
             Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Name: {bossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"Level: {level}");
+            Console.WriteLine($"HP: {currentHp} / {maxHp}");
+            Console.WriteLine($"Attack Power: {attackPower}");
+            Console.WriteLine($"Crit Multiplier: {critMultiplier}");
+            Console.WriteLine($"Is Boos: {true}");
+
             Console.WriteLine();
 
             int hpPercent = currentHp * 100 / maxHp;
             Console.WriteLine($"HP Percent: {hpPercent}%");
             Console.WriteLine();
             Console.WriteLine("Kirin takes 60 damage!");
+            currentHp = currentHp - 60;
+            Console.WriteLine();
+            Console.WriteLine("===== BOSS STATUS: AFTER DAMAGE =====");
+            Console.WriteLine($"HP: {currentHp} / {maxHp}");
+            Console.WriteLine($"HP Percent: {currentHp * 100 / maxHp}%");
         }
     }
 }
